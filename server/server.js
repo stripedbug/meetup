90673abd585cf77396682f62fa969dca48254afd
stripedbug <49484866+stripedbug@users.noreply.github.com>
2024-05-27 17:17:15 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const HTTPS_PORT = 8443;
-
+//const HTTPS_PORT = 8443;
+const HTTPS_PORT = process.env.PORT || 4000;
 const fs = require('fs');
 const https = require('https');
 const WebSocket = require('ws');
